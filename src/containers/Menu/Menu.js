73ea5c6d7ddebb55c6ebe18bb497bec71c041358
Elvis Duru <duruelvis95@gmail.com>
2019-12-19 15:58:02 +0100

    fixed navbar

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import styles from './Menu.module.css';
 
-import { Link } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
 
 import logo from '../../images/logo.png';
 
-export default class Menu extends Component {
+import facebook from '../../images/Facebook-white.svg';
+import twitter from '../../images/Twitter-white.svg';
+import instagram from '../../images/instagram-white.svg';
+import linkedin from '../../images/linkedin-white.svg';
+import pinterest from '../../images/pinterest-white.svg';
+
+import facebookDark from '../../images/Facebook.svg';
+import twitterDark from '../../images/Twitter.svg';
+import instagramDark from '../../images/Instagram.svg';
+import linkedinDark from '../../images/linkedin.svg';
+import pinterestDark from '../../images/pinterest.svg';
+
+class Menu extends Component {
   render() {
     return (
       <nav className={styles.Menu} style={{ color: this.props.color, backgroundColor: this.props.bgColor }}>
@@ -14,16 +26,35 @@ export default class Menu extends Component {
           <p>FC BETHEL <br /> INTERNATIONAL</p>
         </Link>
         <ul className={styles.nav}>
-          <li><Link to="/">home</Link></li>
-          <li><Link to="/about">about us</Link></li>
-          <li><Link to="/players">our players</Link></li>
-          {/* <li><Link to="/staff">our staff</Link></li> */}
-          <li><Link to="/partners">partners</Link></li>
-          {/* <li><Link to="/news">news</Link></li> */}
-          <li><Link to="/gallery">gallery</Link></li>
-          <li><Link to="/contact">contact</Link></li>
+          <li><NavLink to="/" exact >home</NavLink></li>
+          <li><NavLink to="/about">about us</NavLink></li>
+          <li><NavLink to="/players">our players</NavLink></li>
+          {/* <li><NavLink to="/staff">our staff</NavLink></li> */}
+          <li><NavLink to="/partners">partners</NavLink></li>
+          {/* <li><NavLink to="/news">news</NavLink></li> */}
+          <li><NavLink to="/gallery">gallery</NavLink></li>
+          <li><NavLink to="/contact">contact</NavLink></li>
         </ul>
+        <div className={styles.social}>
+          <a href="https://www.facebook.com/uniquebiotechnology.westafrica">
+            <img src={this.props.location.pathname === "/" ? facebook : facebookDark} alt="" />
+          </a>
+          <a href="https://www.twitter.com/uniquebiotechwa">
+            <img src={this.props.location.pathname === "/" ? twitter : twitterDark} alt="" />
+          </a>
+          <a href="https://www.instagram.com/uniquebiotechnologywa">
+            <img src={this.props.location.pathname === "/" ? instagram : instagramDark} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/company/unique-biotechnology-west-africa-limited/">
+            <img src={this.props.location.pathname === "/" ? linkedin : linkedinDark} alt="" />
+          </a>
+          <a href="https://www.pinterest.com/uniquebiotechnologywestafrica/">
+            <img src={this.props.location.pathname === "/" ? pinterest : pinterestDark} alt="" />
+          </a>
+        </div>
       </nav>
     )
   }
 }
+
+export default withRouter(Menu);
